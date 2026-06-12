bde3c9469c602b303f76573bbdbf1a22dcafd92f
Gustavo Koglin <contato@devgustavokoglin.com.br>
2026-06-12 18:08:27 -0400

    feat(UI): add H.NotifyIcon.WinUI system tray menu with quick access, github links and absolute app termination

--- src/DesktopCommandCenter.UI/MainWindow.xaml.cs
+++ src/DesktopCommandCenter.UI/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
+        TrayShowCommand = new CommunityToolkit.Mvvm.Input.RelayCommand(ShowApp);
         InitializeComponent();
         this.Closed += MainWindow_Closed;
 
@@ -47,6 +48,51 @@ public sealed partial class MainWindow : Window
         // Cancel the close, hide instead so the app keeps running in the background for hotkeys
         args.Handled = true;
         this.AppWindow.Hide();
+    }
+
+    public System.Windows.Input.ICommand TrayShowCommand { get; }
+
+    private void ShowApp()
+    {
+        this.AppWindow.Show();
+        WinRT.Interop.WindowNative.GetWindowHandle(this);
+        // Bring to front
+        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+        // (If needed, use P/Invoke SetForegroundWindow, but Show() usually suffices for WinUI 3)
+    }
+
+    private void TrayUpdate_Click(object sender, RoutedEventArgs e)
+    {
+        ShowApp();
+        CommunityToolkit.Mvvm.Messaging.WeakReferenceMessenger.Default.Send(new Messages.NavigateMessage("Settings"));
+    }
+
+    private void TrayShow_Click(object sender, RoutedEventArgs e)
+    {
+        ShowApp();
+    }
+
+    private void TraySettings_Click(object sender, RoutedEventArgs e)
+    {
+        ShowApp();
+        CommunityToolkit.Mvvm.Messaging.WeakReferenceMessenger.Default.Send(new Messages.NavigateMessage("Settings"));
+    }
+
+    private async void TrayDocs_Click(object sender, RoutedEventArgs e)
+    {
+        await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/foreigntechnologies/DesktopCommandCenter-Community"));
+    }
+
+    private async void TrayBug_Click(object sender, RoutedEventArgs e)
+    {
+        await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/foreigntechnologies/DesktopCommandCenter-Community/issues"));
+    }
+
+    private void TrayExit_Click(object sender, RoutedEventArgs e)
+    {
+        // Force the app to close completely
+        Microsoft.UI.Xaml.Application.Current.Exit();
     }
 
     private async void RootFrame_Loaded(object sender, RoutedEventArgs e)
